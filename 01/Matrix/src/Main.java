f0a2a1cc1b629ebd6f654e9b51cb7e8103bb1695
Arkadiy <firecat1987@gmail.com>
2016-10-28 12:22:13 +0300

    ...

--- 01/Matrix/src/Main.java
+++ 01/Matrix/src/Main.java
@@ -1,10 +1,54 @@
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner mScanner = new Scanner(System.in);
+    private static int[][] a;
 
     public static void main(String[] args) {
 
-        int[][] a = new int[4][5];
+        System.out.println("a - автоматическое создание матрицы, m - ручное создание матрицы, exit - выход из программы");
+        if (mScanner.hasNext()) {
+            switch (mScanner.nextLine()) {
+                case "a":
+                    autoMatr();
+                    break;
+                case "m":
+                    manualMatr();
+                    break;
+                case "exit":
+                    System.exit(0);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+
+    private static void manualMatr() {
+        System.out.println("Введите размерность матрицы:");
+        System.out.print("x = ");
+        int mRazmerMatrX = mScanner.nextInt();
+        System.out.print("y = ");
+        int mRazmerMatrY = mScanner.nextInt();
+        a = new int[mRazmerMatrX][mRazmerMatrY];
+        for (int i = 0; i < mRazmerMatrX; i++) {
+            for (int j = 0; j < mRazmerMatrY; j++) {
+                System.out.print("x = " + i + " y = " + j + " ");
+                a[i][j] = mScanner.nextInt();
+            }
+        }
+        printMatr(a);
+        int[][] tA = transMatr(a);
+        printMatr(tA);
+        int[][] mA = multMatr(a, tA);
+        printMatr(mA);
+    }
+
+    private static void autoMatr() {
+        a = new int[4][5];
         int counter = 0;
-        for (int[] ai:a) {
+        for (int[] ai : a) {
             for (int i = 0; i < ai.length; i++) {
                 ai[i] = counter++;
             }
@@ -12,15 +56,15 @@ public class Main {
         printMatr(a);
         int[][] tA = transMatr(a);
         printMatr(tA);
-        int[][] mA = multMatr(a,tA);
+        int[][] mA = multMatr(a, tA);
         printMatr(mA);
     }
 
-    private static int[][] multMatr(int[][] m1, int[][] m2) throws IllegalArgumentException{
-        if(m1.length == 0 && m2.length == 0) {
+    private static int[][] multMatr(int[][] m1, int[][] m2) throws IllegalArgumentException {
+        if (m1.length == 0 && m2.length == 0) {
             throw new IllegalArgumentException("Одна или обе матрицы пустые");
         }
-        if(m1[0].length != m2.length) {
+        if (m1[0].length != m2.length) {
             throw new IllegalArgumentException("Матрицы не согласованы");
         }
         int[][] res = new int[m1.length][m2[0].length];
@@ -36,20 +80,20 @@ public class Main {
 
     }
 
-    private static int[][] transMatr(int[][] m) throws IllegalArgumentException{
+    private static int[][] transMatr(int[][] m) throws IllegalArgumentException {
 
-        if(m.length == 0) {
+        if (m.length == 0) {
             throw new IllegalArgumentException("Матрица пустая");
         }
         int rowLength = m[0].length;
-        for (int[] ai:m) {
+        for (int[] ai : m) {
             if (rowLength != ai.length) {
                 throw new IllegalArgumentException("Матрица не симметрична");
             }
         }
 
         System.out.println("Транспонируем матрицу...");
-        int [][] tM = new int[rowLength][];
+        int[][] tM = new int[rowLength][];
         for (int i = 0; i < rowLength; i++) {
             tM[i] = new int[m.length];
         }
@@ -65,9 +109,9 @@ public class Main {
     }
 
     private static void printMatr(int[][] m) {
-        for (int[] i:m) {
+        for (int[] i : m) {
             String v = "";
-            for (int j:i) {
+            for (int j : i) {
                 v += j + " ";
             }
             System.out.println(v);
