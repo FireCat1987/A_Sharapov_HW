1d6aa49ea2e16ffbbcdb108fd3dff00757acd0a6
Arkadiy <firecat1987@gmail.com>
2016-11-08 19:16:24 +0300

    ...

---
+++ 04/GoodsRegister/src/sample/Model/stockModel.java
@@ -0,0 +1,44 @@
+package sample.Model;
+
+public class stockModel {
+    private int stockId;
+    private String stockName;
+    private String stockDesc;
+
+    public stockModel(int id, String name) {
+        this.stockId = id;
+        this.stockName = name;
+        this.stockDesc = "";
+    }
+
+    public stockModel(int id, String name, String desc) {
+        this.stockId = id;
+        this.stockName = name;
+        this.stockDesc = desc;
+    }
+
+
+    public String getStockName() {
+        return this.stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public int getStockId() {
+        return this.stockId;
+    }
+
+    public void setStockId(int stockId) {
+        this.stockId = stockId;
+    }
+
+    public String getStockDesc() {
+        return this.stockDesc;
+    }
+
+    public void setStockDesc(String stockDesc) {
+        this.stockDesc = stockDesc;
+    }
+}
