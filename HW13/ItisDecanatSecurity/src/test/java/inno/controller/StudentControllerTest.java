c6824e11a9be5a4c6d7bc009e55eb30926be9752
Arkadiy <FireCat1987@gmail.com>
2016-12-20 14:45:08 +0300

    ...

---
+++ HW13/ItisDecanatSecurity/src/test/java/inno/controller/StudentControllerTest.java
@@ -0,0 +1,66 @@
+package inno.controller;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StudentControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void indexPage() throws Exception {
+
+    }
+
+    @Test
+    public void getAllStudents() throws Exception {
+
+    }
+
+    @Test
+    public void addNewStudentPage() throws Exception {
+
+    }
+
+    @Test
+    public void addNewStudent() throws Exception {
+
+    }
+
+    @Test
+    public void showStudent() throws Exception {
+
+    }
+
+    @Test
+    public void deleteScore() throws Exception {
+
+    }
+
+    @Test
+    public void addScoreToStudent() throws Exception {
+
+    }
+
+    @Test
+    public void deleteStudent() throws Exception {
+
+    }
+
+    @Test
+    public void editStudent() throws Exception {
+
+    }
+
+    @Test
+    public void saveStudent() throws Exception {
+
+    }
+
+    @Test
+    public void saveStudent1() throws Exception {
+
+    }
+
+}
